6bc4c8d0a5f63575fac3357bb42cb718569a2fc0
ClifAIsla <clifa.isla@gmail.com>
2022-04-05 13:07:06 -0500

    agregamos list y detalle

--- servidor/controladores/productController.js
+++ servidor/controladores/productController.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const Product = require('./../modelos/productModel');
 
 const createProduct = (request,response) => {
@@ -15,8 +15,33 @@ const createProduct = (request,response) => {
         });
 }
 
+const getAllProducts = (request,response) => {
+    Product.find()
+        .then( allProducts => {
+            return response.status(200).json(allProducts);
+        })
+        .catch( err => {
+            response.statusMessage = "Hubo un error al cargar la lista de docuemtnos."
+            return response.status(404).end();
+        })
+}
+
+const getOneProduct = (request,response) => {
+    const {id} = request.params;
+    Product.findById(id)
+        .then(product => {
+            return response.status(200).json(product);
+        })
+        .catch(err => {
+            console.log(err)
+            return response.status(404).end();
+        })
+}
+
 const ControladorProducts = {
-    createProduct
+    createProduct,
+    getAllProducts,
+    getOneProduct
 }
 
 module.exports = ControladorProducts;
--- servidor/rutas/productRuta.js
+++ servidor/rutas/productRuta.js
@@ -4,5 +4,7 @@ const ProductRouter = express.Router();
 const ControladorProducts = require('./../controladores/productController');
 
 ProductRouter.post('/api/new', ControladorProducts.createProduct);
+ProductRouter.get('/api/allProducts', ControladorProducts.getAllProducts);
+ProductRouter.get('/api/product/:id', ControladorProducts.getOneProduct);
 
 module.exports = ProductRouter;
